bbda57280a84ab2decbd2a4d6352b59fc2af0126
Alina Hrypa <ahry@isd.dp.ua>
2023-05-09 17:00:06 +0300

    display heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {Hero} from "../hero";
+import {HEROES} from "../mock-heroes";
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,16 @@ import {Hero} from "../hero";
 })
 export class HeroesComponent {
 
-  hero: Hero = {
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  heroWind: Hero = {
     id: 1,
     name: 'Windstorm'
   };
+
+  onSelect (hero : Hero) : void {
+    this.selectedHero = hero;
+  }
 
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Dr. Nice'},
+  {id: 2, name: 'Bombasto'},
+  {id: 2, name: 'Celeritas'},
+  {id: 3, name: 'Magneta'}
+];
